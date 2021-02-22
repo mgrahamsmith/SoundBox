42158925b3d275e717511a3605d5a51fb52859d3
Graham Smith <mgrahamsmith@protonmail.com>
2021-02-22 00:19:03 -0700

    Added first steps to sound device interfacing on windows.

--- source/sounds.cpp
+++ source/sounds.cpp
@@ -1,8 +1,20 @@
-
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
+// #include "combaseapi.h"
+#include "mmdeviceapi.h"
+
+// Didn't get very far here, next stop: https://docs.microsoft.com/en-us/windows/win32/coreaudio/rendering-a-stream
+
+/// For Details: https://docs.microsoft.com/en-us/windows/win32/coreaudio/mmdevice-api
 void getAudioDevice()
 {
+    const CLSID CLSID_MMDeviceEnumerator = __uuido(MMDeviceEnumerator);
+    const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+    hr = CoCreateInstance(
+            CLSID_MMDeviceEnumerator, NULL,
+            CLSCTX_ALL, IID_IMMDeviceEnumerator,
+            (void**)&pEnumerator);
+
     return;
 }
